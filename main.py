1bddf203268a11c175f21d8bc21d2b97dd0647a8
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-11 18:23:47 +0400

    Добавлена функция winning_combinations(), генерирующая кортеж выигрышных комбинаций

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Tuple, Set, Any
+
+
+def winning_combinations(dim: int) -> Tuple[Set[Any], ...]:
+    """
+    The function generates a tuple with sets of winning combinations
+    :param dim: Int. Size of game field.
+    :return: Tuple.
+    """
+    combinations = []
+
+    for row in range(dim):
+        combination = set(range(row * dim, (row + 1) * dim))
+        combinations.append(combination)
+
+    for col in range(dim):
+        combination = set(range(col, dim * dim, dim))
+        combinations.append(combination)
+
+    combination = set(range(0, dim * dim, dim + 1))
+    combinations.append(combination)
+
+    combination = set(range(dim - 1, dim * dim - dim + 1, dim - 1))
+    combinations.append(combination)
+
+    return tuple(combinations)
