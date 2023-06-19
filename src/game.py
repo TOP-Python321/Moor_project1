ed3795a3f81be6ff28af1968fb621fe23e46c1fd
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-19 14:46:22 +0400

    Добавлена get_human_turn(): Запрашивает пользовательский ввод для хода до получения корректного ввода.

--- src/game.py
+++ src/game.py
@@ -1,3 +1,22 @@
+import data
+
+
+def get_human_turn() -> int | None:
+    """Requests user input for a move during gameplay. If the input is incorrect, it repeats the request until the correct input is received."""
+    while True:
+        turn = input(data.PROMPT)
+        if not turn:
+            return None
+        try:
+            turn = int(turn)
+        except ValueError:
+            pass
+        else:
+            if 0 <= turn < data.all_cells:
+                if turn not in data.turns:
+                    return turn
+
+
 def game():
     """
     Game process controller
