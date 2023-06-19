6bc904fa3ace49095fdf345d13c4bc09800c30b7
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-19 09:24:37 +0400

    Добавлен цикл while

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import data
 import player
 """
 Точка входа
@@ -12,4 +13,20 @@ player.get_player_name()
 # Суперцикл
 while True:
     # Ожидание ввода команды
-    pass
+    command = input('Введите команду: ')
+
+    if command in data.COMMANDS['начать новую партию']:
+        pass
+    elif command in data.COMMANDS['загрузить существующую партию']:
+        pass
+    elif command in data.COMMANDS['отобразить раздел помощи']:
+        pass
+    elif command in data.COMMANDS['создать или переключиться на игрока']:
+        pass
+    elif command in data.COMMANDS['отобразить таблицу результатов']:
+        pass
+    elif command in data.COMMANDS['изменить размер поля']:
+        pass
+    elif command in data.COMMANDS['выйти']:
+        pass
+
