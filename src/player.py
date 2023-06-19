c9a78f82fa20189988ca41680056f36d35bd3a77
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-19 14:42:20 +0400

    Добавлена update_stats(): Обновляет статистику игроков

--- src/player.py
+++ src/player.py
@@ -16,3 +16,14 @@ def get_player_name() -> None:
         data.player_db[name] = {'побед': 0, 'поражений': 0, 'ничьих': 0}
         utils.write_player()
     data.players += [name]
+
+
+def update_stats(result: list[str]) -> None:
+    """Updates the statistics of active players according to the results of the game."""
+    if result:
+        winner, looser = result
+        data.players_db[winner]['побед'] += 1
+        data.players_db[looser]['поражений'] += 1
+    else:
+        for name in data.players:
+            data.players_db[name]['ничьих'] += 1
