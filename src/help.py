11d840a99159033386734cd240e09c68d74702e7
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-19 14:31:55 +0400

    Добавлена get_help(): Выводит текст помощи

---
+++ src/help.py
@@ -0,0 +1,9 @@
+import data
+import utils
+
+
+def get_help() -> None:
+    print(utils.show_title(data.HELP_TITLE))
+    with open(data.HELP_PATH, 'r', encoding='utf-8') as f:
+        file = f.read()
+    print(file)
