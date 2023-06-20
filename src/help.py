2b9ca92d434c779ce826f6c23be07527ca8785ab
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-20 10:33:46 +0400

    Добавлена документация get_help()

--- src/help.py
+++ src/help.py
@@ -3,6 +3,9 @@ import utils
 
 
 def get_help() -> None:
+    """
+    Displays the 'help' section.
+    """
     print(utils.show_title(data.HELP_TITLE))
     with open(data.HELP_PATH, 'r', encoding='utf-8') as f:
         file = f.read()
