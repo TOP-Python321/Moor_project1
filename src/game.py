77477b5b0cc3f023a3faaa67b61e11ea62ca6e58
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-21 16:19:02 +0400

    начало работы над телом функции game()

--- src/game.py
+++ src/game.py
@@ -1,4 +1,5 @@
 import data
+import utils
 
 
 def get_human_turn() -> int | None:
@@ -21,3 +22,27 @@ def game():
     """
     Game process controller
     """
+    # 9. Цикл до максимального количества ходов
+    for t in range(len(data.turns), data.all_cells):
+        o = t % 2
+
+        ...
+
+        # 10. Запрос хода игрока
+        print(f'Ход игрока {data.players[o]} ')
+        turn = get_human_turn()
+        data.turns[turn] = data.TOKENS[o]
+        # а) ЕСЛИ ввод пустой:
+        if turn is None:
+            # сохранение незавершённой партии
+            data.saves_db[data.players] = data.turns
+            with open(data.SAVES_PATH, 'a', encoding='utf-8') as file:
+                file.write(data.saves_db)
+            # переход к этапу 4
+            return None
+
+        ...
+
+    else:
+        # ничья
+        return []
