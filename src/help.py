4fd1d87c481d0a3d3cc77be518d9265627f2b793
Gennadiy <renardes@gmail.com>
2023-06-24 11:37:24 +0500

    get_help(): комментарии и правки

--- src/help.py
+++ src/help.py
@@ -7,6 +7,8 @@ def get_help() -> None:
     Displays the 'help' section.
     """
     print(utils.show_title(data.HELP_TITLE))
+    # ИСПРАВИТЬ: может, всё же прочитать файл один раз заранее, а не во время каждого вызова функции?)
     with open(data.HELP_PATH, 'r', encoding='utf-8') as f:
         file = f.read()
     print(file)
+
