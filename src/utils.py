f1b011322c7971d1315ddc5d263c1b475b61771b
Schwarzepolizei <schwarzepolizei@gmail.com>
2023-06-26 11:22:44 +0400

    Добавление read_saves()

--- src/utils.py
+++ src/utils.py
@@ -87,6 +87,23 @@ def read_players() -> bool:
     return bool(config)
 
 
+def read_saves() -> None:
+    """"""
+    saves = data.SAVES_PATH.read_text(encoding='utf-8').split('\n')
+    for save in saves:
+        players, turns, dim = save.split('!')
+        data.saves_db |= {
+            tuple(players.split(',')): {
+                'dim': int(dim),
+                'turns': {
+                    int(turn): data.TOKENS[i%2]
+                    for i, turn in enumerate(turns.split(','))
+                },
+            }
+        }
+
+
+
 def write_player() -> None:
     """
     Writes information from the corresponding global data structure to the player data file.
